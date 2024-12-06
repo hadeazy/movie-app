cf3700545833c9aec5401c9d9b2281b55cd7a147
imodoiepale <ijepale@gmail.com>
2024-12-07 00:43:08 +0300

    cv

--- v2/next.config.mjs
+++ v2/next.config.mjs
@@ -1,29 +1,32 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-
-//     images: {
-//     domains: ['image.tmdb.org'],
-//   },
-// };
-
-// export default nextConfig;
-
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
-    distDir: 'dist',
+
     images: {
-        domains: ['image.tmdb.org'],
-        unoptimized: true
+    domains: ['image.tmdb.org'],
     },
     typescript: {
-        ignoreBuildErrors: true
-    },
-    eslint: {
-        ignoreDuringBuilds: true
-    },
-    trailingSlash: true
+            ignoreBuildErrors: true
+        },
 };
 
 export default nextConfig;
+
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//     output: 'export',
+//     distDir: 'dist',
+//     images: {
+//         domains: ['image.tmdb.org'],
+//         unoptimized: true
+//     },
+//     typescript: {
+//         ignoreBuildErrors: true
+//     },
+//     eslint: {
+//         ignoreDuringBuilds: true
+//     },
+//     trailingSlash: true
+// };
+
+// export default nextConfig;
